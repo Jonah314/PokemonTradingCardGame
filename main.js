930d129f0bab766f98aabedbdb2131e5d48a0496
Jonathan T Cantu <Jonathant.cantu@gmail.com>
2025-12-09 23:26:47 -0600

    Added Hp logic, so Hp displays in the hp area

--- main.js
+++ main.js
@@ -3,23 +3,40 @@ import bulbasaurAttacks from "./JavaScriptFolder/javaScript.js";
 import CardBasic from "./JavaScriptFolder/CardBasic.js";
 
 
+let myPokemon = new CardBasic("Pikachu");
+let rivalPokemon = new CardBasic("squirtle");
+let myPokemonHealth = myPokemon.hp - myPokemon.counters;
+let rivalPokemonHealth = rivalPokemon.hp -rivalPokemon.counters;
+
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("attackOne").addEventListener("click", () => {
+        attack1();
+    });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("attackTwo").addEventListener("click", () => {
         setActivePokemon();
     });
 });
 
-let myPokemon = new CardBasic("Pikachu");
 
-
+function attack1(){
+    
+    
+    document.getElementById("activeAreaHp").innerHTML = "its working";
+    document.getElementById("activeHp").innerHTML ="its working";
+}
 
 
 
 function setActivePokemon(){
     console.log(myPokemon.weakness);
     console.log(myPokemon.name);
+    console.log(myPokemonHealth);
+    console.log(rivalPokemonHealth);
     bulbasaurAttacks();
     console.log('testing');
-    document.getElementById("activeAreaHp").innerHTML = "its working";
+    document.getElementById("activeAreaHp").innerHTML = myPokemonHealth + " Hp";
     document.getElementById("activeHp").innerHTML ="its working";
 }
